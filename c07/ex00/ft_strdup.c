073d6238fe6511ec205892ad06b8b58045f6a743
Akihiro Yoshida <ayoshida@c1r22s8.42tokyo.jp>
2020-01-23 20:54:17 +0900

    0123.3

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -6,11 +6,50 @@
 /*   By: ayoshida <ayoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 19:49:03 by ayoshida          #+#    #+#             */
-/*   Updated: 2020/01/23 19:49:18 by ayoshida         ###   ########.fr       */
+/*   Updated: 2020/01/23 20:52:37 by ayoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strdup(char *src)
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	count(char *str)
 {
-	
+	int n;
+
+	while (str[n] != '\0')
+		n++;
+	return (n);
+}
+
+char	*ft_strdup(char *src)
+{
+	int num1;
+	int num2;
+	char *dest;
+
+	num1 = count(src);
+	dest = (char *)malloc(sizeof(char) * num1 + 1);
+	if (dest == NULL)
+		return (NULL);
+	num2 = 0;
+	while (src[num2] != '\0')
+	{
+		dest[num2] = src[num2];
+		num2++;
+	}
+	dest[num2] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char a[] = "123456789";
+	char *b;
+
+	b = ft_strdup(a);
+
+	printf("%s", b);
+	return (0);
 }
