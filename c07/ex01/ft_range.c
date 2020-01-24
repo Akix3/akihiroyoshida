ea6df80b7a9dcea6eb73edfb4ee21f6e50f1efb6
Akihiro Yoshida <ayoshida@c1r19s3.42tokyo.jp>
2020-01-24 13:02:00 +0900

    0124-1

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -1,56 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayoshida <ayoshida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/23 19:49:03 by ayoshida          #+#    #+#             */
-/*   Updated: 2020/01/23 23:01:17 by ayoshida         ###   ########.fr       */
+/*   Created: 2020/01/24 11:59:10 by ayoshida          #+#    #+#             */
+/*   Updated: 2020/01/24 13:00:24 by ayoshida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	count(char *str)
+int	*ft_range(int min, int max)
 {
-	int n;
+	int diff;
+	int count;
+	int *result;
 
-	n = 0;
-	while (str[n] != '\0')
-		n++;
-	return (n);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		num1;
-	int		num2;
-	char	*dest;
-
-	num1 = count(src);
-	dest = (char *)malloc(sizeof(char) * (num1 + 1));
-	if (dest == NULL)
-		return (NULL);
-	num2 = 0;
-	while (src[num2] != '\0')
+	diff = max - min;
+	if (min >= max)
+		return (0);
+	result = (int *)malloc(sizeof(int) * diff + 1);
+	count = 0;
+	while (count <= diff)
 	{
-		dest[num2] = src[num2];
-		num2++;
+		result[count] = min;
+		count++;
+		min++;
 	}
-	dest[num2] = '\0';
-	return (dest);
+	return (result);
 }
 
 int	main(void)
 {
-	char a[10] = "123456789";
-	char *b;
+	int max = 100;
+	int min = 30;
+	int *copy = ft_range(min, max);
+	int num;
 
-	// printf("%c", a[8]);
-	b = ft_strdup(a);
-
-	printf("%s", b);
+	num = 0;
+	while (num < (max - min))
+	{
+		printf("%d\n", copy[num]);
+		num++;
+	}
 	return (0);
 }
