89314a224ccf64677eb335a00badd277aec78abd
Akihiro Yoshida <ayoshida@c1r19s4.42tokyo.jp>
2020-01-27 20:50:20 +0900

    01272049

--- c08/ex04/ft_strs_to_tab_mine.c
+++ c08/ex04/ft_strs_to_tab_mine.c
@@ -1,4 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab_mine.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayoshida <ayoshida@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 20:38:17 by ayoshida          #+#    #+#             */
+/*   Updated: 2020/01/27 20:49:16 by ayoshida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_stock_str.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 int	count(char *src)
 {
@@ -10,20 +24,86 @@ int	count(char *src)
 	return (i);
 }
 
+char	*ft_copy(char *source)
+{
+	int		i;
+	char	*destination;
+
+	i = 0;
+	destination = (char *)malloc(sizeof(char) * count(source) + 1);
+	while (source[i])
+	{
+		destination[i] = source[i];
+	}
+	destination[i] = '\0';
+	return (destination);
+}
+
 struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-	t_stock_str **dest;
-	int num1;
-	int num2;
+	t_stock_str	*dest;
+	int			num;
 
 	dest = (t_stock_str *)malloc(sizeof(t_stock_str) * ac + 1);
-	num1 = 0;
-	while(av[num1])
+	if (!dest)
+		return (dest);
+	num = 0;
+	while (av[num])
 	{
-		num2 = 0;
-		while(av[num1][num2])
-		{
-			
-		}
+		num = 0;
+		dest[num].size = count(av[num]);
+		dest[num].str = av[num];
+		dest[num].copy = ft_copy(av[num]);
+		num++;
 	}
+	return (dest);
+}
+
+
+
+void	put_number(int number)
+{
+	char i;
+
+	if (number != 0)
+	{
+		i = number % 10 + '0';
+		number /= 10;
+		put_number(number);
+		write(1, &i, 1);
+	}
+}
+
+void	put_string(char *s)
+{
+	int iii;
+
+	iii = 0;
+	while (s[iii])
+		iii++;
+	write(1, s, iii - 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int ii;
+
+	ii = 0;
+	par = (t_stock_str *)malloc(sizeof(t_stock_str) * 1000);
+	while (par + ii)
+	{
+		put_string(par[ii].str);
+		write(1, "\n", 1);
+		put_number(par[ii].size);
+		write(1, "\n", 1);
+		put_string(par[ii].copy);
+		write(1, "\n", 1);
+		ii++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	ft_show_tab(ft_strs_to_tab(argc, argv));
+	return (0);
 }
