2bea348da9e8b25b3b6f4b5f474a39148a2e8f46
吉田 明広 <yoshida@MacBook-Pro.local>
2020-01-27 09:38:18 +0900

    0126-09

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define EVEN(X) (X % 2 == 0)
+#define TRUE 0
+#define FALSE 1
+#define SUCCESS 1
+typedef int t_bool;
